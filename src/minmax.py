14e4dd25dc2457c0c8586a2fe447a6859322e48d
Emilia <120570826+Kissaniemi@users.noreply.github.com>
2024-12-20 12:13:36 +0200

    Update minmax.py

--- src/minmax.py
+++ src/minmax.py
@@ -1,35 +1,41 @@
 import connect
 import time
 
-column_order = [3,2,4,1,5,6,0]  # preferred order of columns to make a move
+# preferred order of columns to make a move to
+column_order = [3, 2, 4, 1, 5, 6, 0]
+
 
 def iterative_deepening(board, player, last_move, alpha, beta):
     start_time = time.time()
-    time_limit = 2    
-    depth_limit = 43  #Maxdepth it could potentially reach if it had enough time
+    time_limit = 2
+    # Maximum depth deepening could (potentially) reach if it had enough time
+    depth_limit = 43
 
     for max_depth in range(1, depth_limit):
-        best_move, value = minmax(board, player, last_move, alpha, beta, max_depth, 0)
+        best_move, value = minmax(
+            board, player, last_move, alpha, beta, max_depth, 0)
 
-        if time.time() - start_time > time_limit or value == 10000 or value == -10000:  
+        if time.time() - start_time > time_limit or value == 10000 or value == -10000:
             break
-        
-    print(f"time taken for move {time.time() - start_time}")
+
+    print(f"approximate time taken for move: {time.time() - start_time }")
+    print(f"depth reached: {max_depth}")
+    print(f"move made to column: {best_move}, move value: {value}")
+
     return best_move, value
 
 
-
-def minmax(board, player, last_move, alpha, beta, max_depth, start_depth):
+def minmax(board, player, last_move, alpha, beta, max_depth, current_depth):
     """MinMax algorithm, returns best move and its value
 
     Args:
         board (nested list): gameboard
         player (int): player whose move we are evaluating
-        last move (int): the last move made
+        last move (int): the last move made by the other player
         alpha (float/int): alpha value for pruning
         beta (float/int): beta value for pruning
-        depth (int, optional): search depth.
-
+        max_depth (int): depth limit (most likely never reached)
+        current_depth: current depth we are on
     """
 
     if last_move is not None:
@@ -42,22 +48,24 @@ def minmax(board, player, last_move, alpha, beta, max_depth, start_depth):
 
     columns = connect.possible_columns(board, column_order)
 
-    if max_depth == start_depth or len(columns) == 0:
+    if current_depth == max_depth or len(columns) == 0:
         if len(columns) == 0:
             return last_move, 0
         value = connect.score_position(board, player)
         return last_move, value
 
-    if player == 2:  # Max player
+    # Max player
+    if player == 2:
         value = -100000
         best_move = None
-        for column in columns:  
+        for column in columns:
             row = connect.check_top(board, column)
-            board[row-1][column] = 2 
-            _, new_value= minmax(board, 1, column, alpha, beta, max_depth, start_depth +1)
+            board[row-1][column] = 2
+            _, new_value = minmax(board, 1, column, alpha,
+                                  beta, max_depth, current_depth + 1)
             board[row-1][column] = 0
 
-            if new_value > value:           
+            if new_value > value:
                 value = new_value
                 best_move = column
             alpha = max(alpha, value)
@@ -69,10 +77,11 @@ def minmax(board, player, last_move, alpha, beta, max_depth, start_depth):
     # Min player
     value = 100000
     best_move = None
-    for column in columns: 
+    for column in columns:
         row = connect.check_top(board, column)
         board[row-1][column] = 1
-        _, new_value = minmax(board, 2, column, alpha, beta, max_depth, start_depth +1)
+        _, new_value = minmax(board, 2, column, alpha,
+                              beta, max_depth, current_depth + 1)
         board[row-1][column] = 0
 
         if new_value < value:
