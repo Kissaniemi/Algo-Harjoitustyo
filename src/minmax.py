8e0e99a17d6fd7352216e382977cd8f50dca7e82
Emilia <120570826+Kissaniemi@users.noreply.github.com>
2025-01-22 17:37:10 +0200

    cache update minmax.py

--- src/minmax.py
+++ src/minmax.py
@@ -5,27 +5,28 @@ import time
 column_order = [3, 2, 4, 1, 5, 6, 0]
 
 
-def iterative_deepening(board, player, last_move, alpha, beta):
+def iterative_deepening(board, player, last_move, alpha, beta, turn):
     start_time = time.time()
     time_limit = 2
-    # Maximum depth deepening could (potentially) reach if it had enough time
-    depth_limit = 43
-
+    depth_limit = 43-turn   #43-game turn Maximum depth deepening could (potentially) reach if it had enough time
+    
     for max_depth in range(1, depth_limit):
+        board_cache = {}
         best_move, value = minmax(
-            board, player, last_move, alpha, beta, max_depth, 0)
+            board, player, last_move, alpha, beta, max_depth, 0, board_cache)
+        
 
         if time.time() - start_time > time_limit or value == 10000:
             break
 
-    print(f"approximate time taken for move: {time.time() - start_time }")
-    print(f"depth reached: {max_depth}")
-    print(f"move made to column: {best_move}, move value: {value}")
+    print(f"Approximate time taken for move: {time.time() - start_time }s")
+    print(f"Depth reached: {max_depth}/{depth_limit}")
+    print(f"Move made to column: {best_move+1}, move value: {value}")
 
     return best_move, value
 
 
-def minmax(board, player, last_move, alpha, beta, max_depth, current_depth):
+def minmax(board, player, last_move, alpha, beta, max_depth, current_depth, board_cache):
     """MinMax algorithm, returns best move and its value
 
     Args:
@@ -37,22 +38,30 @@ def minmax(board, player, last_move, alpha, beta, max_depth, current_depth):
         max_depth (int): depth limit (most likely never reached)
         current_depth: current depth we are on
     """
-
+ 
+    board_hash = hash(str(board))
+    if board_hash in board_cache:
+        #print("board found")
+        return board_cache[board_hash]
+     
     if last_move is not None:
         if player == 1:
             if connect.check_winner(board, last_move, 2):
-                return last_move, 10000
+                return (last_move, 10000)
         else:
             if connect.check_winner(board, last_move, 1):
-                return last_move, -10000
+                return (last_move, -10000)
 
     columns = connect.possible_columns(board, column_order)
 
     if current_depth == max_depth or len(columns) == 0:
         if len(columns) == 0:
-            return last_move, 0
+            board_cache[board_hash] = (last_move, 0)
+            return (last_move, 0)
+        
         value = connect.score_position(board, player)
-        return last_move, value
+        board_cache[board_hash] = (last_move, value)
+        return (last_move, value)
 
     # Max player
     if player == 2:
@@ -62,7 +71,7 @@ def minmax(board, player, last_move, alpha, beta, max_depth, current_depth):
             row = connect.check_top(board, column)
             board[row-1][column] = 2
             _, new_value = minmax(board, 1, column, alpha,
-                                  beta, max_depth, current_depth + 1)
+                                  beta, max_depth, current_depth + 1, board_cache)
             board[row-1][column] = 0
 
             if new_value > value:
@@ -71,8 +80,8 @@ def minmax(board, player, last_move, alpha, beta, max_depth, current_depth):
             alpha = max(alpha, value)
             if alpha >= beta:
                 break
-
-        return best_move, value
+        board_cache[board_hash] = (best_move, value)
+        return (best_move, value)
 
     # Min player
     value = 100000
@@ -81,7 +90,7 @@ def minmax(board, player, last_move, alpha, beta, max_depth, current_depth):
         row = connect.check_top(board, column)
         board[row-1][column] = 1
         _, new_value = minmax(board, 2, column, alpha,
-                              beta, max_depth, current_depth + 1)
+                              beta, max_depth, current_depth + 1, board_cache)
         board[row-1][column] = 0
 
         if new_value < value:
@@ -90,5 +99,5 @@ def minmax(board, player, last_move, alpha, beta, max_depth, current_depth):
         beta = min(beta, value)
         if beta <= alpha:
             break
-
-    return best_move, value
+    board_cache[board_hash] = (best_move, value)
+    return (best_move, value)
